de24511eaed7f56430c0ba5b3fc89732f037dc31
Karthi-Asrix <karthi@asrix.com>
2024-05-21 15:46:34 +0800

    Update script.py
    Changing model_url to localhost

--- script.py
+++ script.py
@@ -22,7 +22,7 @@ from utils import doc_load, append_doc_log
 
 
 class RAGLlm(BaseModel):
-    model_url: str | None = Field(default="http://103.251.2.10:5000")
+    model_url: str | None = Field(default="http://localhost:5000")
     context: str | None = Field(default='data/orient-context.pdf', description="PDF file path on local. (eg. orient-context.pdf, webermeyer-context.pdf)")
     prompt: str | None = Field(default=None, description="User input")
 
@@ -32,7 +32,7 @@ class RAGLlm(BaseModel):
 
 
 class RAGLlm2(BaseModel):
-    model_url: str | None = Field(default="http://103.251.2.10:5000")
+    model_url: str | None = Field(default="http://localhost:5000")
     character_id: str | None = Field(default="OT-PLC/150424022018")
     contexts: List[str] | None = Field(default=[
         "https://gist.githubusercontent.com/EdwardRayl/3436572afde8ce9e3faf5b7b95356a49/raw/6b25895fce480713560829dec31ac8220ffe5272/gists.txt",
